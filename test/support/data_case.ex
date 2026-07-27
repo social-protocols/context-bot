6fe2a7ff9498547394c696ead838a386d83a81d1
Jonathan R. Warden <john.warden@gmail.com>
2026-07-27 17:43:54 -0600

    style: satisfy strict Credo checks

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,9 @@ defmodule ContextBot.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias ContextBot.Repo
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias ContextBot.Repo
@@ -36,8 +39,8 @@ defmodule ContextBot.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(ContextBot.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
